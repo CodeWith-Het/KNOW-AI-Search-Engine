c7892656ea547a4294a36267dc1dc61c4127f2fd
CodeWith-Het <hetp1065@gmail.com>
2026-06-17 00:37:31 -0500

    cont...

--- backend/src/controllers/chats.controller.js
+++ backend/src/controllers/chats.controller.js
@@ -2,7 +2,7 @@ import { generateChatTitle, generateResponse } from "../service/ai.service.js"
 import chatModel from './../models/chat.model.js';
 import messageModel from './../models/message.model.js';
 
-export const sendMessage = async (req, res, next) => {
+export const sendMessage = async (req, res) => {
     try {
         const { message,chat:chatId } = req.body
 
@@ -24,7 +24,7 @@ export const sendMessage = async (req, res, next) => {
         role:"user"
     })
 
-    const messages = await messageModel.find({ chat: chatId } || chat._id);
+    const messages = await messageModel.find({ chat: chatId || chat._id });
         
     const aiResponse = await generateResponse(messages)
 
@@ -47,4 +47,48 @@ export const sendMessage = async (req, res, next) => {
             error: error.message
         })
     }
+}
+
+export const getChats = async (req,res)=>{
+    try{
+        const user = req.user.id
+
+        const chats = await chatModel.find({user:user})
+
+        res.status(200).json({
+            message:"chats fetched successfully",
+            chats
+        })
+    }
+    catch(error){
+        res.status(500).json({
+            message:"server crash",
+            error:error.message
+        })
+    }
+}
+
+export const getMessage = async (req,res)=>{
+  try{
+    const { chatId } = req.params
+
+    if(!chatId){
+        return res.status(404).json({
+            message:"chat not found"
+        })
+    }
+
+    const messages = await messageModel.find({ chat: chatId })
+
+    res.status(200).json({
+        message:"messages fetched successfully",
+        messages
+    })
+  }
+  catch(error){
+    res.status(500).json({
+        message:"server crash",
+        error:error.message
+    })
+  }
 }
--- backend/src/routers/chats.routes.js
+++ backend/src/routers/chats.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { authUser } from '../middleware/auth.middleware.js';
-import { sendMessage } from '../controllers/chats.controller.js';
+import { getChats, getMessage, sendMessage } from '../controllers/chats.controller.js';
 
 const chatRouter = Router()
 
 chatRouter.post("/message",authUser,sendMessage)
+chatRouter.get("/",authUser,getChats)
+chatRouter.get("/:chatid/messages",authUser,getMessage)
 
 export default chatRouter
