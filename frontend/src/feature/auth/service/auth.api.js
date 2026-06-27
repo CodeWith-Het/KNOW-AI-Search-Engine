8500a9385e09dee3c674dd0e3b29571959832ed3
CodeWith-Het <hetp1065@gmail.com>
2026-06-27 11:22:06 -0500

    cont...

--- frontend/src/feature/auth/service/auth.api.js
+++ frontend/src/feature/auth/service/auth.api.js
@@ -1,42 +1,42 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = axios.create({
     baseURL: `${import.meta.env.VITE_API_URL}/api/auth`,
-    withCredentials:true,
-})
+    withCredentials: true,
+});
 
 export const register = async ({ username, email, password }) => {
     try {
-        const response = await api.post("/register", { username, email, password })
-        return response.data
+        const response = await api.post("/register", { username, email, password });
+        return response.data;
     } catch (error) {
-        throw new Error(error.response?.data?.message || "Registration Failed", { cause: error })
+        throw new Error(error.response?.data?.message || "Registration Failed", { cause: error });
     }
-}
+};
 
 export const login = async ({ loginId, password }) => {
     try {
-        const response = await api.post("/login", { loginId, password })
-        return response.data.user
+        const response = await api.post("/login", { loginId, password });
+        return response.data.user;
     } catch (error) {
-        throw new Error(error.response?.data?.message || "Login Failed", { cause: error })
+        throw new Error(error.response?.data?.message || "Login Failed", { cause: error });
     }
-}
+};
 
 export const getUser = async () => {
     try {
-        const response = await api.get("/getuser")
-        return response.data.user
+        const response = await api.get("/getuser");
+        return response.data.user;
     } catch (error) {
-        throw new Error(error.response?.data?.message || "User not retrieved", { cause: error })
+        throw new Error(error.response?.data?.message || "User not retrieved", { cause: error });
     }
-}
+};
 
 export const logout = async () => {
     try {
-        const response = await api.get("/logout")
-        return response.data
+        const response = await api.get("/logout");
+        return response.data;
     } catch (error) {
-        throw new Error(error.response?.data?.message || "Logout Failed", { cause: error })
+        throw new Error(error.response?.data?.message || "Logout Failed", { cause: error });
     }
-}
+};
