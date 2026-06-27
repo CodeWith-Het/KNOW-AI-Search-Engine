1c0e683ec7fb7820e8e0952fc402c97f73a8f435
CodeWith-Het <hetp1065@gmail.com>
2026-06-27 08:11:22 -0500

    refactor CORS configuration to use environment variable for frontend URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,10 +16,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(morgan("dev"))
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://perplexity-ai-umber.vercel.app"
-    ],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
 }));
