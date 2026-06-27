69252b39555590d63107a8c4dde9b7daf2ab9d98
CodeWith-Het <hetp1065@gmail.com>
2026-06-27 08:27:35 -0500

    refactor createTransporter function to specify SMTP host and port for Gmail

--- backend/src/service/mail.service.js
+++ backend/src/service/mail.service.js
@@ -10,7 +10,8 @@ const OAuth2 = google.auth.OAuth2;
 const createTransporter = async () => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
       auth: {
         user: process.env.GOOGLE_USER,
         pass:process.env.GOOGLE_APP_PASSWORD
