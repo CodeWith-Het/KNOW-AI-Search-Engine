f0e7ad1037f13a0fdfa11ab6ea1800af29e2f5b6
CodeWith-Het <hetp1065@gmail.com>
2026-06-27 07:08:40 -0500

    chagne email method

--- backend/src/service/mail.service.js
+++ backend/src/service/mail.service.js
@@ -9,27 +9,11 @@ const OAuth2 = google.auth.OAuth2;
 
 const createTransporter = async () => {
   try {
-    const oauth2Client = new OAuth2(
-      process.env.GOOGLE_CLIENT_ID,
-      process.env.GOOGLE_CLIENT_SECRET,
-      "https://developers.google.com/oauthplayground"
-    );
-
-    oauth2Client.setCredentials({
-      refresh_token: process.env.REFRESH_TOKEN,
-    });
-
-    const accessToken = await oauth2Client.getAccessToken();
-
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        type: "OAuth2",
         user: process.env.GOOGLE_USER,
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
-        accessToken: accessToken?.token || accessToken,
+        password:process.env.GOOGLE_APP_PASSWORD
       },
     });
 
